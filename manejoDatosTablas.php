3ffe57638bac0907b5286375e8671b97217eeeec
Red <90118139+JoAzar@users.noreply.github.com>
2025-09-24 21:12:54 -0300

    Update manejoDatosTablas.php

--- manejoDatosTablas.php
+++ manejoDatosTablas.php
@@ -2,12 +2,13 @@
     require_once '../revisarConexion.php';
     
     function Conexion() {
-        $servername = "localhost";
-        $username = "root";
-        $password = "mag021$";
-        $database = "empleados";
+        $servername = "";
+        $username = "";
+        $password = "";
+        $database = "";
         $conexion = mysqli_connect($servername, $username, $password, $database);
         
         return $conexion;  
     }   
+
 ?>
